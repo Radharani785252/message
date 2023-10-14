e0dea967a8bef0b459f8997e14463caf55ff4dd5
Radharani785252 <Radharani785252@gmail.com>
2023-10-14 15:31:16 +0530

    added one bulk mess

--- index.js
+++ index.js
@@ -39,6 +39,37 @@ app.post("/sendsms", async (req, res) => {
   }
 });
 
+//Bulk message  to send login
+
+app.post("/sendbulksms", async (req, res) => {
+  const { numbers, body } = req.body;
+
+  if (!Array.isArray(numbers) || numbers.length === 0) {
+    return res.status(400).json({ error: "Invalid or empty 'numbers' array" });
+  }
+
+  try {
+    const sentMessages = [];
+
+    for (const number of numbers) {
+      const message = await client.messages.create({
+        body,
+        to: number,
+        from: twilioPhoneNumber,
+      });
+
+      console.log(`Message sent with SID: ${message.sid}`);
+      sentMessages.push({ number, sid: message.sid });
+    }
+
+    res.status(200).json({ message: "SMS sent successfully", sentMessages });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to send SMS" });
+  }
+});
+
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
